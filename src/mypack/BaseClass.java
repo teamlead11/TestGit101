1eec112eae8c006891a27a87f004e24fb90f7584
teamlead11 <automationlead11@gmail.com>
2018-12-06 10:25:08 +0530

    project added with base class

---
+++ src/mypack/BaseClass.java
@@ -0,0 +1,10 @@
+package mypack;
+
+public class BaseClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my base class");
+	}
+
+}
