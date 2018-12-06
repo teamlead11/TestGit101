b51ac0a6cd1218dafef8b7a7c3b4f924e65dbe5f
teamlead11 <automationlead11@gmail.com>
2018-12-06 10:34:53 +0530

    modified base class

--- src/mypack/BaseClass.java
+++ src/mypack/BaseClass.java
@@ -4,7 +4,8 @@ public class BaseClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("this is my base class");
+
+		System.out.println("test");
 	}
 
 }
