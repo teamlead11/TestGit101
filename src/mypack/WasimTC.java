5fa2854b18974334643355ca80d4a847e4af48a4
wasimlms <wasiml.ms@gmail.com>
2018-12-06 18:32:24 +0530

    wasimtc updated

---
+++ src/mypack/WasimTC.java
@@ -0,0 +1,16 @@
+package mypack;
+
+public class WasimTC {
+
+	public static void main(String[] args) {
+		System.out.println("gituuuu");
+		System.out.println("Jib");
+
+		System.out.println("greens");
+		System.out.println("greens1");
+		System.out.println("greens2");
+		System.out.println("greens3");
+
+	}
+
+}
