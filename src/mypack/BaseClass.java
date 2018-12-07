d6e5c361f0263cb4ecb4fe3e4ac16a187e13bb8b
subashudaya <subashudaya20@gmail.com>
2018-12-07 14:28:45 +0530

    test

--- src/mypack/BaseClass.java
+++ src/mypack/BaseClass.java
@@ -1,11 +1,14 @@
 package mypack;
 
 public class BaseClass {
-
+	 public void test1() {
+		 System.out.println("subash");
+	 }
 	public static void main(String[] args) {
 		int a=10,b=20,c;
 		System.out.println(c=a+b);
-		
+		BaseClass base=new BaseClass();
+		base.test1();
 	}
 
 }
