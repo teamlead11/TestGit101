30058ad94eec3c1dc88c2df82eb072629853e443
bharath38595 <bharath38595@gmail.com>
2018-12-07 15:54:49 +0530

    by bharath

--- src/mypack/BaseClass.java
+++ src/mypack/BaseClass.java
@@ -1,11 +1,24 @@
-package mypack;
+ package mypack;
 
 public class BaseClass {
+	
+	int a=10,b=20,c;
+	
+	public void addBharath() {
+		// TODO Auto-generated method stub
+
+	}
 
 	public static void main(String[] args) {
-		int a=10,b=20,c;
+		
 		System.out.println(c=a+b);
 		System.out.println("project class");
+		System.out.println(c);
+		
+		
+	
+		
+		
 	}
 
 }
