e26bcaa5b004090f8f78516ccafa4d0049046a6d
teamlead11 <automationlead11@gmail.com>
2018-12-12 10:16:22 +0530

    completed 10%

---
+++ src/mypack/TestCaseNew.java
@@ -0,0 +1,10 @@
+package mypack;
+
+public class TestCaseNew {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/mypack/TestCasenew1.java
@@ -0,0 +1,13 @@
+package mypack;
+
+public class TestCasenew1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		add(5, 5);
+	}
+
+	public static void add(int a, int b) {
+		System.out.println(a + b);
+	}
+}
