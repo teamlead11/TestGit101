bd726776e006407ad800c1f1409bdbdd46999900
teamlead11 <automationlead11@gmail.com>
2018-12-12 10:19:10 +0530

    completed new test case

---
+++ src/mypack/TestCaseNew1.java
@@ -0,0 +1,13 @@
+package mypack;
+
+public class TestCaseNew1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void add(int a, int b, int c) {
+		System.out.println(a + b + c);
+	}
+}
