cdd7bf7d436bf1a26ea95079960cc953c525c8ec
wasimlms <wasiml.ms@gmail.com>
2018-12-19 16:26:03 +0530

    add the document

--- src/mypack/BaseClass.java
+++ src/mypack/BaseClass.java
@@ -3,6 +3,27 @@ package mypack;
 import java.util.Scanner;
 
 public class BaseClass {
+	public void reverseStringBywasim() {
+		String s = "suriya";
+		StringBuilder sb = new StringBuilder(s);
+		System.out.println("REVERSE STRING : " + sb.reverse().toString());
+	}
+
+	public void reversestringbywasim() {
+		String original, reverse = "";
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Enter a string to reverse");
+		original = in.nextLine();
+
+		int length = original.length();
+
+		for (int i = length - 1; i >= 0; i--)
+			reverse = reverse + original.charAt(i);
+
+		System.out.println("Reverse of the string: " + reverse);
+
+	}
 
 	public void reverseStringBySuriya() {
 		String s = "suriya";
